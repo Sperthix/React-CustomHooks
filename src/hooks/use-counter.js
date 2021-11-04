7c12b2fd993c732b87b7b0922c9df99e66817a54
Sperthix <matusbuci@gmail.com>
2021-11-04 09:28:19 +0100

    starting project

---
+++ src/hooks/use-counter.js
@@ -0,0 +1,22 @@
+import { useState, useEffect } from "react";
+
+const useCounter = (add = true) => {
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+        if (add) {
+            setCounter((prevCounter) => prevCounter + 1);
+        }
+        else {
+            setCounter((prevCounter) => prevCounter - 1);
+        }
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [add]);
+
+  return counter;
+};
+
+export default useCounter;
