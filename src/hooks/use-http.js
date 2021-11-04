1c8a3a9ece4e6ef470de1430355a7850f9b58326
Sperthix <matusbuci@gmail.com>
2021-11-04 14:52:05 +0100

    using custom hook for http request

---
+++ src/hooks/use-http.js
@@ -0,0 +1,36 @@
+import { useState } from "react";
+
+const useHttp = (config, apply) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const sendRequest = async () => {
+    setIsLoading(true);
+    setError(null);
+    try {
+      const response = await fetch(config.URL, {
+        method: config.method ? config.method : "GET",
+        headers: config.headers ? config.headers : {},
+        body: config.body ? JSON.stringify(config.body) : null,
+      });
+
+      if (!response.ok) {
+        throw new Error("Request failed!");
+      }
+
+      const data = await response.json();
+      apply(data);
+    } catch (err) {
+      setError(err.message || "Something went wrong!");
+    }
+    setIsLoading(false);
+  };
+
+  return {
+    isLoading,
+    error,
+    sendRequest,
+  };
+};
+
+export default useHttp;
